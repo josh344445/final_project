b0614f52de2002e7ef4b51270945b35e1c653295
josh344445 <joshuamosesmccoy@gmail.com>
2018-07-17 16:54:34 -0700

    none

--- finding_data.py
+++ finding_data.py
@@ -9,14 +9,17 @@ with open("graffiti_requests.csv") as f:
     column = {}
     for h in headers:
         column[h] = []
-
+    row_number = 0
     for row in reader:
         for h, v in zip(headers, row):
             if h in ('lat', 'long'):
                 column[h].append(float(v.strip()))
             else:
                 column[h].append(v)
-    locations = np.column_stack((column['lat'], column['long']))
-    fig = gmaps.figure()
-    fig.add_layer(gmaps.heatmap_layer(locations, point_radius=20))
-    fig
+            row_number +=1
+            if row_number>100:
+                break
+locations = np.column_stack((column['lat'], column['long']))
+fig = gmaps.figure()
+fig.add_layer(gmaps.heatmap_layer(locations, point_radius=200))
+fig
